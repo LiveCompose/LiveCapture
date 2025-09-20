27eeddc7c0a2686d71e0e4a81f99444b64c51e29
JettyCoffee <jetty_coffee@outlook.com>
2025-09-20 23:28:05 +0800

    Initial Commit

---
+++ LiveCapture/LiveCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCaptureApp.swift
+//  LiveCapture
+//
+//  Created by JettyCoffee on 2025/9/20.
+//
+
+import SwiftUI
+
+@main
+struct LiveCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
